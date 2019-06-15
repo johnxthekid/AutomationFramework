c9aa0a151c59b7ed1cca70968fa6bdeac59548f9
johnxthekid <KreyeYo!52%>
2019-06-15 18:38:59 -0400

    added sample property and browser element action

---
+++ lib/browsers/BrowserElementActions.py
@@ -0,0 +1,44 @@
+class ElementActions:
+
+    def __init__(self, browser, element_instance):
+        """
+        Customs actions that an element can perform on the application
+        :param element_instance:
+        """
+        self.browser = browser
+        self.element = browser.find_element(element_instance)
+
+    def get_element(self, element_locator_type, element_value):
+        """
+        Returns the element base on the provide property value
+        :param element_locator_type: the locator type to use to find the element
+        :param element_value: the value of the element
+        :return: the located element
+        """
+        locator_type = {"id": self.browser.find_element_by_id,
+                        "name": self.browser.find_element_by_name,
+                        "xpath": self.browser.find_element_by_xpath,
+                        "css": self.browser.find_element_by_css_selector,
+                        "class": self.browser.find_element_by_class_name,
+                        "tag": self.browser.find_element_by_tag_name,
+                        "link": self.browser.find_element_by_link_text,
+                        "partial_link": self.browser.find_element_by_partial_link_text}
+
+        return locator_type[element_locator_type](element_value)
+
+    def get_elements(self, element_locator_type, element_value):
+        """
+        Returns the element base on the provide property value
+        :param element_locator_type: the locator type to use to find the element
+        :param element_value: the value of the element
+        :return: the located elements
+        """
+        locator_type = {"name": self.browser.find_elements_by_name,
+                        "xpath": self.browser.find_elements_by_xpath,
+                        "css": self.browser.find_elements_by_css_selector,
+                        "class": self.browser.find_elements_by_class_name,
+                        "tag": self.browser.find_elements_by_tag_name,
+                        "link": self.browser.find_elements_by_link_text,
+                        "partial_link": self.browser.find_elements_by_partial_link_text}
+
+        return locator_type[element_locator_type](element_value)
